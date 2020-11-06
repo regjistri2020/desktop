b5227f6ff125f11ff69b8997148d8112ca9d459c
luis kateshi <luisluiluis2015@outlook.com>
2020-11-06 10:09:04 +0100

    test3

--- DesktopApp/Form1.Designer.cs
+++ DesktopApp/Form1.Designer.cs
@@ -34,7 +34,7 @@
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(414, 230);
+			this.button1.Location = new System.Drawing.Point(45, 111);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(75, 23);
 			this.button1.TabIndex = 0;
@@ -43,7 +43,7 @@
 			// 
 			// dateTimePicker1
 			// 
-			this.dateTimePicker1.Location = new System.Drawing.Point(502, 101);
+			this.dateTimePicker1.Location = new System.Drawing.Point(45, 60);
 			this.dateTimePicker1.Name = "dateTimePicker1";
 			this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
 			this.dateTimePicker1.TabIndex = 1;
@@ -52,7 +52,7 @@
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(810, 450);
 			this.Controls.Add(this.dateTimePicker1);
 			this.Controls.Add(this.button1);
 			this.Name = "Form1";
