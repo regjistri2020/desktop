e02fe6fe30c36bad0c64693b7ab27ff3a41f0f85
Martin <koci.martin2017@gmail.com>
2020-12-05 01:51:10 +0100

    .

---
+++ DesktopApp/Martin/AdminDashboardUC.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace DesktopApp.Martin
+{
+    partial class AdminDashboardUC
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdminDashboardUC));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tittlelabel = new System.Windows.Forms.Label();
+            this.Datapanel = new System.Windows.Forms.Panel();
+            this.DatatextBox = new System.Windows.Forms.TextBox();
+            this.Datalabel = new System.Windows.Forms.Label();
+            this.NxenestextBox = new System.Windows.Forms.TextBox();
+            this.MesuestextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.Datapanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(190)))), ((int)(((byte)(212)))));
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.tittlelabel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(931, 38);
+            this.panel1.TabIndex = 0;
+
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DesktopApp.Properties.Resources.icons8_back_arrow_64;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(34, 30);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+  
+            // 
+            // tittlelabel
+            // 
+            this.tittlelabel.AutoSize = true;
+            this.tittlelabel.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tittlelabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(8)))), ((int)(((byte)(63)))), ((int)(((byte)(70)))));
+            this.tittlelabel.Location = new System.Drawing.Point(43, 7);
+            this.tittlelabel.Name = "tittlelabel";
+            this.tittlelabel.Size = new System.Drawing.Size(118, 21);
+            this.tittlelabel.TabIndex = 2;
+            this.tittlelabel.Text = "Faqja Kryesore";
+
+            // 
+            // Datapanel
+            // 
+            this.Datapanel.Controls.Add(this.DatatextBox);
+            this.Datapanel.Controls.Add(this.Datalabel);
+            this.Datapanel.Controls.Add(this.NxenestextBox);
+            this.Datapanel.Controls.Add(this.MesuestextBox);
+            this.Datapanel.Controls.Add(this.label3);
+            this.Datapanel.Controls.Add(this.label1);
+            this.Datapanel.Controls.Add(this.pictureBox4);
+            this.Datapanel.Controls.Add(this.pictureBox2);
+            this.Datapanel.Controls.Add(this.panel3);
+            this.Datapanel.Controls.Add(this.panel2);
+            this.Datapanel.Controls.Add(this.label2);
+            this.Datapanel.Controls.Add(this.pictureBox3);
+            this.Datapanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Datapanel.Location = new System.Drawing.Point(0, 38);
+            this.Datapanel.Name = "Datapanel";
+            this.Datapanel.Size = new System.Drawing.Size(931, 603);
+            this.Datapanel.TabIndex = 1;
+            // 
+            // DatatextBox
+            // 
+            this.DatatextBox.BackColor = System.Drawing.SystemColors.Control;
+            this.DatatextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.DatatextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DatatextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.DatatextBox.Location = new System.Drawing.Point(121, 19);
+            this.DatatextBox.Name = "DatatextBox";
+            this.DatatextBox.Size = new System.Drawing.Size(123, 22);
+            this.DatatextBox.TabIndex = 17;
+            this.DatatextBox.Text = "07.12.2020";
+            this.DatatextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Datalabel
+            // 
+            this.Datalabel.AutoSize = true;
+            this.Datalabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Datalabel.ForeColor = System.Drawing.Color.Red;
+            this.Datalabel.Location = new System.Drawing.Point(46, 14);
+            this.Datalabel.Name = "Datalabel";
+            this.Datalabel.Size = new System.Drawing.Size(80, 29);
+            this.Datalabel.TabIndex = 16;
+            this.Datalabel.Text = "Data: ";
+            // 
+            // NxenestextBox
+            // 
+            this.NxenestextBox.BackColor = System.Drawing.SystemColors.Control;
+            this.NxenestextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.NxenestextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NxenestextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.NxenestextBox.Location = new System.Drawing.Point(688, 182);
+            this.NxenestextBox.Name = "NxenestextBox";
+            this.NxenestextBox.Size = new System.Drawing.Size(100, 31);
+            this.NxenestextBox.TabIndex = 15;
+            this.NxenestextBox.Text = "305";
+            this.NxenestextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // MesuestextBox
+            // 
+            this.MesuestextBox.BackColor = System.Drawing.SystemColors.Control;
+            this.MesuestextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.MesuestextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MesuestextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.MesuestextBox.Location = new System.Drawing.Point(268, 185);
+            this.MesuestextBox.Name = "MesuestextBox";
+            this.MesuestextBox.Size = new System.Drawing.Size(100, 31);
+            this.MesuestextBox.TabIndex = 14;
+            this.MesuestextBox.Text = "21";
+            this.MesuestextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(687, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 29);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Nxënës";
+
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(267, 143);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 29);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Mësues";
+
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(557, 139);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(92, 91);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 11;
+            this.pictureBox4.TabStop = false;
+
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(133, 133);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(102, 105);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(190)))), ((int)(((byte)(212)))));
+            this.panel3.Location = new System.Drawing.Point(34, 303);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(850, 5);
+            this.panel3.TabIndex = 9;
+
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(190)))), ((int)(((byte)(212)))));
+            this.panel2.Location = new System.Drawing.Point(34, 53);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(644, 5);
+            this.panel2.TabIndex = 8;
+
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(79)))), ((int)(((byte)(37)))), ((int)(((byte)(122)))));
+            this.label2.Location = new System.Drawing.Point(684, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 21);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Rifresko të dhënat";
+
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::DesktopApp.Properties.Resources.restart_48px;
+            this.pictureBox3.Location = new System.Drawing.Point(833, 37);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(40, 31);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+
+            // 
+            // AdminDashboardUC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.Datapanel);
+            this.Controls.Add(this.panel1);
+            this.Name = "AdminDashboardUC";
+            this.Size = new System.Drawing.Size(931, 641);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Datapanel.ResumeLayout(false);
+            this.Datapanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label tittlelabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel Datapanel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox DatatextBox;
+        private System.Windows.Forms.Label Datalabel;
+        private System.Windows.Forms.TextBox NxenestextBox;
+        private System.Windows.Forms.TextBox MesuestextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
