a3aa24766e05214c514a07c87e13247bf11617c0
Martin <koci.martin2017@gmail.com>
2020-12-08 21:07:12 +0100

    Privacy

--- DesktopApp/Martin/PrivacyPolicy.Designer.cs
+++ DesktopApp/Martin/PrivacyPolicy.Designer.cs
@@ -84,7 +84,7 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(562, 405);
+            this.button1.Location = new System.Drawing.Point(554, 402);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 8;
