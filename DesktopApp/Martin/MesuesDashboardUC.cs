555b63bd4f2ae350563180e1da597436a100a867
Martin <koci.martin2017@gmail.com>
2021-03-11 10:25:05 +0100

    ...

--- DesktopApp/Martin/MesuesDashboardUC.cs
+++ DesktopApp/Martin/MesuesDashboardUC.cs
@@ -35,27 +35,27 @@ namespace DesktopApp.Martin
         private void KlasatextBox_TextChanged(object sender, EventArgs e)
         {
 
-            KlasatextBox.Text = CookieClass.Klasa;
+            //KlasatextBox.Text = CookieClass.Klasa;
 
-            KlasatextBox.Clear();
-            var connectionString = "server=remotemysql.com;userid=gBh6InugME;password=NSGsLG2ITM;database=gBh6InugME";
-            using (var connection = new MySqlConnection(connectionString))
-            {
-                connection.Open();
-                var query = "SELECT KlasaID FROM Klasa where Emri = '" + KlasatextBox.Text + "'";
-                using (var command = new MySqlCommand(query, connection))
-                {
-                    using (var reader = command.ExecuteReader())
-                    {
-                        //Iterate through the rows and add it to the combobox's items
-                        while (reader.Read())
-                        {
-                            CookieClass.KlasaID = reader.GetString("KlasaID");
+            //KlasatextBox.Clear();
+            //var connectionString = "server=remotemysql.com;userid=gBh6InugME;password=NSGsLG2ITM;database=gBh6InugME";
+            //using (var connection = new MySqlConnection(connectionString))
+            //{
+            //    connection.Open();
+            //    var query = "SELECT KlasaID FROM Klasa where Mesuesesi = '" + KlasatextBox.Text + "'";
+            //    using (var command = new MySqlCommand(query, connection))
+            //    {
+            //        using (var reader = command.ExecuteReader())
+            //        {
+            //            //Iterate through the rows and add it to the combobox's items
+            //            while (reader.Read())
+            //            {
+            //                CookieClass.KlasaID = reader.GetString("KlasaID");
                             
-                        }
-                    }
-                }
-            }
+            //            }
+            //        }
+            //    }
+            //}
 
         }
     }
